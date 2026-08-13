8bbcb05bd7ff97b34c0223cf200e7ebf2b3df130
Mark Tristan C. Garcia <147889724+Tristan8100@users.noreply.github.com>
2026-08-13 21:57:21 +0800

    refactor: clean up environment variables and enhance main application setup for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; //ADDED
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from "@nestjs/platform-express" //for ip address
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); //ADDED
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,           // Strips properties not in DTO
+      forbidNonWhitelisted: true, // Throws error if extra properties sent (recommended)
+      transform: true,            // Auto-transform payloads to DTO instances
+    })); //ADDED
   app.enableCors({
-    origin: 'http://localhost:3000', // your frontend URL
+    origin: process.env.FRONTEND_URL || '*', // your frontend URL
     credentials: true, // if sending cookies or auth headers
   });
-
-  const config = new DocumentBuilder()
-    .setTitle('Authentication API')
-    .setDescription('A starter kit for NestJS authentication with MongoDB.')
-    .setVersion('1.0')
-    .addBearerAuth() // This enables JWT authorization in Swagger
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document); // This sets up the Swagger UI at /api-docs
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
