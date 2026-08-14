b84629e9dcc3d9aa5e5d755c85436e861613d10b
Mark Tristan C. Garcia <147889724+Tristan8100@users.noreply.github.com>
2026-08-14 19:17:52 +0800

    reconfigure swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; //ADDED
 import { NestExpressApplication } from "@nestjs/platform-express" //for ip address
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('trust proxy', 1);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe({
       whitelist: true,           // Strips properties not in DTO
